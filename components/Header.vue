58ee7d76d91ec65d7a722eaaa917c977ed42d807
AntonioPivaldi <9212565@gmail.com>
2022-12-20 12:35:14 +0300

    feat: add mobile catalogue tree

--- components/Header.vue
+++ components/Header.vue
@@ -13,11 +13,72 @@
 
         </div>
       </div>
-      <nav :class="`header__navbar${burgerShown ? '' : '-hidden'}`">
+      <nav :class="`header__navbar${burgerShown ? '' : '-hidden'} header__navbar-desktop`">
         <li class="link-li" v-for="item of headerNavLinks" :key="item.text" @click="currentWidth <= 960 ? burgerButton() : '';">
           <nuxt-link :to="item.link" class="link-custom">{{ item.text }}</nuxt-link>
         </li>
       </nav>
+      <div class="structure-wrapper" :style="``">
+        <section
+          :style="`grid-template-columns: repeat(${maxDepth + 1}, 100%); transform: translateX(-${currentColumn * 100}%); transition: 0.5s ease;`"
+          class="structure-mobile"
+          v-if="categoriesTree && !$fetchState.pending && burgerShown"
+        >
+          <nav :class="`header__navbar${burgerShown ? '' : '-hidden'}`">
+            <li class="link-li" v-for="item of headerNavLinks" :key="item.text">
+              <div class="link-custom" style="cursor: pointer;" v-if="item.catalogue" @click="currentColumn = 1">{{ item.text }}</div>
+              <div v-else
+                @click="currentWidth <= 960 ? burgerButton() : ''"
+                >
+                <nuxt-link :to="item.link" class="link-custom">{{ item.text }}</nuxt-link>
+              </div>
+            </li>
+          </nav>
+          <div
+            class="structure__column"
+            v-for="(column, index) in renderColumns"
+            :key="index"
+          >
+            <div class="structure__column-wrapper">
+              <div
+                v-if="index === 0"
+                class="structure__item fade-in structure__item-all"
+                style="font-weight: 600"
+                @click="routeWithQuery(),
+                setColumnData(index + 1, [])"
+              >
+                <span>Все товары</span>
+              </div>
+              <div v-else class="structure__backward"
+              @click="currentColumn = index">
+                <img src="/category-item-arrow.svg" alt="backward arrow" style="transform: rotate(180deg)">
+                <div>{{ backwardElement[index] }}</div>
+              </div>
+              <div
+                :class="{ 'structure__item-active': activeItems.includes(item._id) }"
+                class="structure__item fade-in"
+                v-for="item in column"
+                @click="setColumnData(index + 1, item.children, item._id),
+                item.children.length ? '' : routeWithQuery(item._id),
+                currentColumn = (index < maxDepth - 1? index + 2 : index + 1),
+                backwardElement[index + 1] = item?.name
+                debuger(currentColumn)"
+                :key="item.name"
+              >
+                <span>{{ item?.name }}</span>
+                <img v-if="item.children?.length" src="/category-item-arrow.svg" :alt="item?.name">
+              </div>
+              <div v-if="index === 0"
+                @click="currentColumn = 0"
+                class="structure__backward"
+              >
+                <img src="/category-item-arrow.svg" alt="backward arrow" style="transform: rotate(180deg)">
+                <div>назад</div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
       <div :class="`overlay${burgerShown ? '' : '-hidden'}`">
       </div>
       <Search class="search" />
@@ -26,50 +87,136 @@
 </template>
 
 <script scoped>
-export default {
-  data: () => {
-    return {
-      headerNavLinks: [
-      { text: 'ГЛАВНАЯ', link: '/'},
-      { text: 'КАТАЛОГ', link: '/catalogue'},
-      { text: 'О НАС', link: '/about'},
-      { text: 'АКЦИИ', link: '/offers' },
-      { text: 'ТРЕКЕР ЗАКАЗА', link: '/tracker'},
-      { text: 'КОРЗИНА', link: '/cart'},
-      ],
-      burgerShown: false,
-      currentHeight: 0,
-      currentWidth: 0,
-    }
-  },
 
-  methods: {
-    burgerButton() {
-      this.burgerShown = !this.burgerShown;
-      document.body.style.overflow = this.burgerShown && this.currentWidth <= 960 ? 'hidden' : 'visible';
+  export default {
+    data: () => {
+      return {
+        headerNavLinks: [
+        { text: 'ГЛАВНАЯ', link: '/'},
+        { text: 'КАТАЛОГ', link: '/catalogue', catalogue: true},
+        { text: 'О НАС', link: '/about'},
+        { text: 'АКЦИИ', link: '/offers' },
+        { text: 'ТРЕКЕР ЗАКАЗА', link: '/tracker'},
+        { text: 'КОРЗИНА', link: '/cart'},
+        ],
+        burgerShown: false,
+        currentHeight: 0,
+        currentWidth: 0,
+        renderColumns: [],
+        activeItems: [],
+        currentColumn: 0,
+        backwardElement: [],
+      }
     },
-    getCurrentScreenSize() {
-      this.currentHeight = window.innerHeight;
-      this.currentWidth = window.innerWidth;
-    }
-  },
 
-  beforeMount() {
-    window.addEventListener('resize', this.getCurrentScreenSize)
-  },
+    computed: {
+      categoriesTree () {
+        return this.$store.state.categories.categoriesTree;
+      },
+      maxDepth () {
+        return this.$store.state.categories.maxDepth;
+      },
+    },
+    async fetch() {
+      this.renderColumns = Array(this.maxDepth).fill([]);
+      this.activeItems = Array(this.maxDepth).fill(null);
+      this.renderColumns[0] = this.categoriesTree?.children || [];
+    },
 
-  mounted() {
-    this.$root.$on('burgerButton', () => {
-      this.burgerButton();
-    });
-    this.getCurrentScreenSize();
-  },
-}
+    methods: {
+      burgerButton() {
+        this.burgerShown = !this.burgerShown;
+        document.body.style.overflow = this.burgerShown && this.currentWidth <= 960 ? 'hidden' : 'visible';
+      },
+      getCurrentScreenSize() {
+        this.currentHeight = window.innerHeight;
+        this.currentWidth = window.innerWidth;
+      },
+      setColumnData(depth, data, itemId) {
+        this.activeItems[depth] = null;
+        for (let index = depth + 1; index < this.maxDepth; index++) {
+          this.renderColumns[index] = [];
+          this.backwardElement[index] = [];
+          this.activeItems[index] = null;
+        }
+        if (depth < this.maxDepth) {
+          this.renderColumns[depth] = data;
+          if (itemId) {
+            this.activeItems[depth] = itemId;
+            this.backwardElement[depth] = this.renderColumns[itemId];
+          }
+          this.renderColumns.push([]);
+          this.renderColumns.pop();
+        }
+      },
+      debuger: function(el) {
+        console.log(el)
+      },
+      routeWithQuery(category) {
+        this.$router.push({path: '/products', query: { category }})
+      },
+    },
+
+    beforeMount() {
+      window.addEventListener('resize', this.getCurrentScreenSize)
+    },
+
+    mounted() {
+      this.$root.$on('burgerButton', () => {
+        this.burgerButton();
+      });
+      this.getCurrentScreenSize();
+    },
+  }
 </script>
 
 
 <style lang="scss" scoped>
   @import '~/assets/styles/global';
+
+  .structure {
+
+    &-wrapper {
+      display: none;
+
+      @include breakpoint(l) {
+        display: flex;
+        position: relative;
+        z-index: 100;
+        width: 15rem;
+        overflow: hidden;
+      }
+    }
+
+    &-mobile {
+      display: grid;
+      align-items: baseline;
+    }
+
+    &__item {
+      display: flex;
+      width: 100%;
+      max-width: 15rem;
+      font-size: 1.25rem;
+      justify-content: space-between;
+
+      &:hover {
+        opacity: 0.9;
+      }
+    }
+
+    &__backward {
+      display: flex;
+      gap: 1rem;
+      font-weight: 600;
+      font-size: 1.25rem;
+      cursor: pointer;
+
+      &:last-child {
+        font-weight: 500;
+      }
+    }
+  }
 
   .search-mobile {
     display: none;
@@ -194,12 +341,9 @@ export default {
       }
 
       @include breakpoint(l) {
-        position: fixed;
-        z-index: 100;
         flex-direction: column;
         align-items: center;
         width: fit-content;
-        top: 15rem;
         padding: 0.5rem;
         height: calc(16rem);
         transition: 0.5s ease;
@@ -209,6 +353,14 @@ export default {
         & li {
           font-size: 1.5rem;
           line-height: 1.5rem;
+        }
+      }
+
+      &-desktop {
+        @extend .header__navbar;
+
+        @include breakpoint(l) {
+          display: none
         }
       }
 
