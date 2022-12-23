cd27e08a72dc632281627c8505cbdc2c627c8302
zidiks <zidiks228@gmail.com>
2022-12-23 17:53:36 +0300

    feat: number filter

--- components/Inputs/MobileNumberInput.vue
+++ components/Inputs/MobileNumberInput.vue
@@ -1,21 +1,32 @@
 <template>
-  <div class="mobile-filter__item">
-    <span class="mobile-filter__item__label">{{ property.name }}</span>
-    <div class="mobile-filter__item__action">
-
-    </div>
+  <div class="mobile-filter__range">
+    <span class="mobile-filter__range__label">{{ property.name }} ОТ {{ value[0] + postfix }} до {{ value[1] + postfix }}</span>
+    <a-slider :tooltipVisible="false" :tooltip-visible="true" v-model:value="value" range :step="10" />
   </div>
 </template>
 
 <script>
 import {ComparisonOperator} from "assets/shared/enums/mongoose-query.enum";
+import {ref} from "vue";
 
 export default {
   props: ['form', 'property'],
   data() {
     return {
       ComparisonOperator,
+      value: ref([20, 50]),
     }
   },
+  computed: {
+    postfix() {
+      return this.property.code === 'totalPrice' ? ' BYN' : '';
+    }
+  },
+  watch: {
+    value(value) {
+      this.form[this.property.code][ComparisonOperator.gte] = value[0];
+      this.form[this.property.code][ComparisonOperator.lte] = value[1];
+    }
+  }
 }
 </script>
