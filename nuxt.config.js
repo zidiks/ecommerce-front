6443d945e7d41125b4eb169e35634f91a6162879
Zidiks <zidiks228@gmail.com>
2022-12-26 21:10:42 +0300

    fix: host

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,10 +60,6 @@ export default {
   build: {
   },
 
-  server: {
-    host: '192.168.0.106'
-  },
-
   publicRuntimeConfig: {
     env: environment,
     baseUrl: `${environment.https ? 'https' : 'http'}://${environment.host}:${environment.port}`
