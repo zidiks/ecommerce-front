3699677174091476d18c5d767763b3bb0bd1bb30
Zidiks <zidiks228@gmail.com>
2022-12-28 19:10:12 +0300

    fix: ultra ssl

--- environments/environment.js
+++ environments/environment.js
@@ -2,7 +2,7 @@ export const environmentDev = {
   production: false,
   host: '92.255.111.60',
   port: 3000,
-  https: false,
+  https: true,
   httpRequestTimeout: 30000,
   debounceTime: 400,
 };
